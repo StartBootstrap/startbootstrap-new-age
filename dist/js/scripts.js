3219f5fb6a9d0155e2496281a35bf0d7f1307879
David Miller <hello@davidmiller.io>
2021-08-05 16:03:18 -0400

    Bootstrap 5.1.0 Update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - New Age v6.0.3 (https://startbootstrap.com/theme/new-age)
+* Start Bootstrap - New Age v6.0.4 (https://startbootstrap.com/theme/new-age)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
 */
